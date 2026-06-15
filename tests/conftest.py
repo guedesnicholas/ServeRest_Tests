abde6f025f2c664b541d99ad5a39d7c3aae10971
Nicholas <nicholasguedes04@gmail.com>
2026-06-15 17:31:30 -0300

    feat: obter token de usuários admin e não admin

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 import pytest
 import sys
 import os
+import uuid
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 
 from src.api.usuarios_client import UsuariosClient
-from src.api.usuarios_client import ProdutosClient
+from src.api.produtos_client import ProdutosClient
 from src.helpers.factories import payload_usuario_valido
 from src.helpers.factories import payload_produto_valido
 
@@ -44,20 +45,68 @@ def pclient():
 
 
 @pytest.fixture
-def produto_cadastrado(pclient):
-    #fornecer um usuário real na API
-    #É criado e destruído a cada teste que o usa 
+def produto_cadastrado(pclient, token): 
+    # fornecer um produto real na API
+    # é criado e destruído a cada teste que o usa
     payload = payload_produto_valido()
 
-    resposta = pclient.cadastrar(payload)
+    resposta = pclient.cadastrar(payload, token)  
 
     assert resposta.status_code == 201, (
-        f"Falha ao criar usuário. Status: {resposta.status_code} — {resposta.text}"
+        f"Falha ao criar produto. Status: {resposta.status_code} — {resposta.text}"
     )
 
-    usuario_id = resposta.json()["_id"]
+    produto_id = resposta.json()["_id"]
 
-    yield {"id": usuario_id, "payload": payload}
+    yield {"id": produto_id, "payload": payload}
 
-    # Cleanup: remove o usuário criado
+    # Cleanup
+    pclient.excluir(produto_id, token)  
+
+
+@pytest.fixture(scope="session")
+def token(client):
+    # Arrange
+    payload = {
+        "nome": "Usuario Admin",
+        "email": f"auth_{uuid.uuid4().hex[:8]}@qa.com",
+        "password": "senha123",
+        "administrador": "true",
+    }
+    criacao = client.cadastrar(payload)
+    usuario_id = criacao.json()["_id"]
+
+    # Act
+    resposta = client.login({
+        "email": payload["email"],
+        "password": payload["password"],
+    })
+
+    token = resposta.json()["authorization"]
+
+    yield token  # entrega o token para os testes
+
+    # Cleanup
+    client.excluir(usuario_id)
+
+@pytest.fixture(scope="session")
+def token_nao_admin(client):
+    payload = {
+        "nome": "Usuario Comum",
+        "email": f"comum_{uuid.uuid4().hex[:8]}@qa.com",
+        "password": "senha123",
+        "administrador": "false",  
+    }
+    criacao = client.cadastrar(payload)
+    usuario_id = criacao.json()["_id"]
+
+    resposta = client.login({
+        "email": payload["email"],
+        "password": payload["password"],
+    })
+
+    token = resposta.json()["authorization"]
+
+    yield token
+
     client.excluir(usuario_id)
